e778d40edeee37f833bcaab32e4bd41dc39df075
Victor-Barrio <victorbarrio82@gmail.com>
2025-01-27 11:57:39 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,19 +22,19 @@ const joinBtn = document.querySelector(".joinUs__form-button");
 // Creating the object with the info
 const contentData = [
   {
-    image: "../images/illustration-features-tab-1.svg",
+    image: "./images/illustration-features-tab-1.svg",
     title: "Bookmark in one click",
     paragraph:
       "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
   },
   {
-    image: "../images/illustration-features-tab-2.svg",
+    image: "./images/illustration-features-tab-2.svg",
     title: "Intelligent Search",
     paragraph:
       "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
   },
   {
-    image: "../images/illustration-features-tab-3.svg",
+    image: "./images/illustration-features-tab-3.svg",
     title: "Share your bookmarks",
     paragraph:
       "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
